9a2d7bff667b01dca24591e601ea7cced3461192
maryjios <mari67jos@gmail.com>
2021-02-19 02:38:25 -0500

    Vista del login terminada

---
+++ app/src/main/java/com/example/agroplazaappmovil/Login.java
@@ -0,0 +1,17 @@
+package com.example.agroplazaappmovil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.login);
+
+
+
+    }
+}
--- app/src/main/java/com/example/agroplazaappmovil/MainActivity.java
+++ app/src/main/java/com/example/agroplazaappmovil/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.agroplazaappmovil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_main);
+
+        TimerTask tarea = new TimerTask() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(MainActivity.this, Login.class);
+
+                startActivity(intent);
+
+                finish();
+            }
+        };
+
+        Timer tiempo = new Timer();
+        tiempo.schedule(tarea, 5000);
     }
 }
