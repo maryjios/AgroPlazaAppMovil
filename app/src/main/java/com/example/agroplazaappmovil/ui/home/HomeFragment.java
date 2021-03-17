c65274a1cb37a0a20dd1870ffc3e79cd91050ca3
nmaryjios <75151380+maryjios@users.noreply.github.com>
2021-03-17 16:34:26 -0500

    Agregando contenido a acticvidad <inicio>

--- app/src/main/java/com/example/agroplazaappmovil/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/agroplazaappmovil/ui/home/HomeFragment.java
@@ -1,5 +1,7 @@
 package com.example.agroplazaappmovil.ui.home;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,15 +14,18 @@ import androidx.fragment.app.Fragment;
 import com.example.agroplazaappmovil.R;
 
 public class HomeFragment extends Fragment {
-
     View actividad;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         actividad = inflater.inflate(R.layout.fragment_home, container, false);
 
-        TextView etiqueta = actividad.findViewById(R.id.text_home);
-        etiqueta.setText("Modificado desde el fragmento ^w^");
+        SharedPreferences persistencia = actividad.getContext().getSharedPreferences("datos_login", Context.MODE_PRIVATE);
 
+        TextView nombreUser = actividad.findViewById(R.id.textBienvenidaPincipal);
+        nombreUser.setText("Hola, "+""+persistencia.getString ("nombres", "").toString ()+","+" Bienvenido");
+        /*
+        TextView ubicacion = actividad.findViewById(R.id.textlUbicacion);
+        ubicacion.setText("Ahora mismo estas en - Pereira,Risaralda"); */
         return actividad;
     }
 }
