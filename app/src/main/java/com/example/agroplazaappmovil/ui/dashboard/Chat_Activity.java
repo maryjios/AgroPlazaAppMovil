9fd4a41911394f45479a73230b0bce865c9d9c79
Julian Espinosa <jaet1509@outlook.com>
2021-04-02 17:47:45 -0500

    Conexion con mqtt para enviar y recibir mensajes establecido

--- app/src/main/java/com/example/agroplazaappmovil/ui/dashboard/Chat_Activity.java
+++ app/src/main/java/com/example/agroplazaappmovil/ui/dashboard/Chat_Activity.java
@@ -2,14 +2,45 @@ package com.example.agroplazaappmovil.ui.dashboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.example.agroplazaappmovil.R;
 import com.example.agroplazaappmovil.ui.home.DetallePublicacion;
 
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.DisconnectedBufferOptions;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class Chat_Activity extends AppCompatActivity {
+
+    private static final String SERVER_URI = "ws://18.221.49.32:8083/mqtt";
+    private static final String TOPIC = "prueba_chat";
+    private static final String TAG = "PruebaMqtt";
+
+    private MqttAndroidClient mqttAndroidClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +54,150 @@ public class Chat_Activity extends AppCompatActivity {
                 Chat_Activity.super.onBackPressed ();
             }
         });
+
+        SharedPreferences persistencia = getSharedPreferences("datos_login", Context.MODE_PRIVATE);
+        String client_id = "user_id_" + persistencia.getString("id", "NaN");
+
+        conexionMqtt(client_id);
+
+        EditText txt_mensage = findViewById(R.id.new_mensaje);
+
+        ImageView enviar_mensaje = findViewById(R.id.btn_enviar_mensaje);
+        enviar_mensaje.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[] nom_usuario = persistencia.getString("nombres", "NaN").split(" ");
+                String[] ape_usuario = persistencia.getString("apellidos", "NaN").split(" ");
+                String usuario = nom_usuario[0] + " " + ape_usuario[0];
+
+                String mensaje = txt_mensage.getText().toString();
+                txt_mensage.setText("");
+
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+                Date date = new Date();
+                String fecha = dateFormat.format(date) + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+                String avatar = persistencia.getString("avatar", "NaN");
+
+                JSONObject datos_mensaje = new JSONObject();
+                try {
+                    datos_mensaje.put("id", client_id);
+                    datos_mensaje.put("usuario", usuario);
+                    datos_mensaje.put("mensaje", mensaje);
+                    datos_mensaje.put("fecha", fecha);
+                    datos_mensaje.put("avatar", avatar);
+
+                    Log.i(TAG, datos_mensaje.toString());
+                    publishMessage(datos_mensaje);
+                    mostrarMensaje(datos_mensaje);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+    }
+
+    public void conexionMqtt(String client_id) {
+        Log.i(TAG, "onCreate: client_Id: " + client_id);
+
+        mqttAndroidClient = new MqttAndroidClient(getApplicationContext(), SERVER_URI, client_id);
+        mqttAndroidClient.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean reconnect, String serverURI) {
+                if (reconnect) {
+                    Log.i(TAG, "Reconnected to : " + serverURI);
+                    // Because Clean Session is true, we need to re-subscribe
+                    subscribeToTopic();
+                } else {
+                    Log.i(TAG, "Connected to: " + serverURI);
+                }
+            }
+
+            @Override
+            public void connectionLost(Throwable cause) {
+                Log.e(TAG, "The Connection was lost.");
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                Log.i(TAG, "Incoming message: " + new String(message.getPayload()));
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+
+            }
+        });
+
+        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+        mqttConnectOptions.setAutomaticReconnect(true);
+        mqttConnectOptions.setCleanSession(false);
+
+        try {
+            //addToHistory("Connecting to " + serverUri);
+            mqttAndroidClient.connect(mqttConnectOptions, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    DisconnectedBufferOptions disconnectedBufferOptions = new DisconnectedBufferOptions();
+                    disconnectedBufferOptions.setBufferEnabled(true);
+                    disconnectedBufferOptions.setBufferSize(100);
+                    disconnectedBufferOptions.setPersistBuffer(false);
+                    disconnectedBufferOptions.setDeleteOldestMessages(false);
+                    mqttAndroidClient.setBufferOpts(disconnectedBufferOptions);
+                    subscribeToTopic();
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.e(TAG, "Failed to connect to: " + SERVER_URI);
+                }
+            });
+
+
+        } catch (MqttException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public void subscribeToTopic(){
+        try {
+            mqttAndroidClient.subscribe(TOPIC, 0, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Log.i(TAG, "Subscribed!");
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.e(TAG, "Failed to subscribe");
+                }
+            });
+
+        } catch (MqttException ex){
+            Log.e(TAG, "Exception whilst subscribing");
+            ex.printStackTrace();
+        }
+    }
+
+    public void publishMessage(JSONObject mensaje){
+
+        try {
+            MqttMessage message = new MqttMessage();
+            message.setPayload(mensaje.toString().getBytes());
+            mqttAndroidClient.publish(TOPIC, message);
+            Log.i(TAG, "Message Published");
+            if(!mqttAndroidClient.isConnected()){
+                Log.i(TAG, mqttAndroidClient.getBufferedMessageCount() + " messages in buffer.");
+            }
+        } catch (MqttException e) {
+            Log.e(TAG, "Error Publishing: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void mostrarMensaje(JSONObject datos) {
+        RelativeLayout mensage = new RelativeLayout(this);
+
     }
 }
