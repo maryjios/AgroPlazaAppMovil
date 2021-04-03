6912236d558a8d9b4f6bbbdc9afe61a3d15e7643
nmaryjios <75151380+maryjios@users.noreply.github.com>
2021-04-02 19:44:31 -0500

    Asignado recycler view a activity chat

---
+++ app/src/main/java/com/example/agroplazaappmovil/ui/dashboard/Mensajes.java
@@ -0,0 +1,20 @@
+package com.example.agroplazaappmovil.ui.dashboard;
+
+public class Mensajes {
+    // String avatar_destinario;
+    String nombre_destinario;
+    String mensaje_destinario;
+    String fecha_destinario;
+    String mensaje_remitente;
+    String fecha_remitente;
+
+
+    public Mensajes (String nombre_destinario, String mensaje_destinario, String fecha_destinario, String mensaje_remitente, String fecha_remitente) {
+        // this.avatar_destinario = avatar_destinario;
+        this.nombre_destinario = nombre_destinario;
+        this.mensaje_destinario = mensaje_destinario;
+        this.fecha_destinario = fecha_destinario;
+        this.mensaje_remitente = mensaje_remitente;
+        this.fecha_remitente = fecha_remitente;
+    }
+}
