fb9fea8636fc0d0d159922dae1169cbf420e0639
nmaryjios <75151380+maryjios@users.noreply.github.com>
2021-04-05 00:06:25 -0500

    Detalle pedido terminado en un 99%

--- app/src/main/java/com/example/agroplazaappmovil/ui/dashboard/DetallePedido.java
+++ app/src/main/java/com/example/agroplazaappmovil/ui/dashboard/DetallePedido.java
@@ -1,21 +1,37 @@
 package com.example.agroplazaappmovil.ui.dashboard;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.agroplazaappmovil.R;
 import com.example.agroplazaappmovil.ui.perfil.EditarDatos;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class DetallePedido extends AppCompatActivity {
     String id_pedido, titulo_pedido, fecha_pedido, estado_pedido;
-
+    TextView nombre_consumidor, id_consumidor, direccion_consumidor, telefono_consumidor;
+    TextView TituloProducto, precioPedido, envioPedido, vendedor, textoPrecioUnit, textoCantidad, textoDescuento, textoTotal;
+    Button verPerfilVendedor;
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
@@ -26,6 +42,11 @@ public class DetallePedido extends AppCompatActivity {
         titulo_pedido = intent.getStringExtra ("titulo");
         fecha_pedido = intent.getStringExtra ("fecha");
         estado_pedido = intent.getStringExtra ("estado");
+
+        nombre_consumidor = findViewById (R.id.nombreCliente);
+        id_consumidor = findViewById (R.id.cedulaCliente);
+        direccion_consumidor = findViewById (R.id.direccionCliente);
+        telefono_consumidor = findViewById (R.id.telefonoCliente);
 
         LinearLayout cPedidoEntregado = findViewById (R.id.contentPedidoEntregado);
         if (estado_pedido.equalsIgnoreCase ("ENTREGADO")){
@@ -52,5 +73,69 @@ public class DetallePedido extends AppCompatActivity {
                 DetallePedido.super.onBackPressed();
             }
         });
+
+        llenarDatosDetallePedido();
+        llenarDatosConsumidorDetallePedido();
+    }
+
+    public void llenarDatosDetallePedido () {
+
+        TituloProducto = findViewById (R.id.TituloProducto);
+        precioPedido = findViewById (R.id.precioPedido);
+        envioPedido = findViewById (R.id.envioPedido);
+        verPerfilVendedor = findViewById (R.id.verPerfilVendedor);
+        textoPrecioUnit = findViewById (R.id.textoPrecioUnit);
+        textoCantidad = findViewById (R.id.textoCantidad);
+        textoDescuento = findViewById (R.id.textoDescuento);
+        textoTotal = findViewById (R.id.textoTotal);
+
+        RequestQueue hilo = Volley.newRequestQueue (getApplicationContext ());
+        String url = "https://agroplaza.solucionsoftware.co/ModuloPedidos/DatosDetallePedido?pedido=" + id_pedido;
+
+        JsonObjectRequest solicitud = new JsonObjectRequest (Request.Method.GET, url, null, new Response.Listener<JSONObject> () {
+            @Override
+            public void onResponse (JSONObject response) {
+                JSONArray datosDetalle = response.optJSONArray ("datos");
+
+                try {
+                    for (int i = 0; i < datosDetalle.length (); i++) {
+                        JSONObject dato = datosDetalle.getJSONObject (i);
+                        TituloProducto.setText (dato.getString ("titulo_p"));
+                        precioPedido.setText (dato.getString ("precio_p"));
+                        if (dato.getString ("envio_p").equalsIgnoreCase ("NO")){
+                            envioPedido.setVisibility (View.GONE);
+                        }else {
+                            envioPedido.setVisibility (View.VISIBLE);
+                        }
+                        verPerfilVendedor.setText (dato.getString ("nombre_vendedor"));
+                        textoPrecioUnit.setText (dato.getString ("precio_p"));
+                        textoCantidad.setText (dato.getString ("cantidad_p"));
+                        textoDescuento.setText ("$"+dato.getString ("descuento_p"));
+                        textoTotal.setText (dato.getString ("total_p"));
+
+
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace ();
+                }
+            }
+        }, new Response.ErrorListener () {
+            @Override
+            public void onErrorResponse (VolleyError error) {
+
+            }
+        });
+
+        hilo.add(solicitud);
+    }
+
+    public void llenarDatosConsumidorDetallePedido () {
+
+        SharedPreferences persistencia = getApplicationContext ().getSharedPreferences("datos_login", Context.MODE_PRIVATE);
+        nombre_consumidor.setText (persistencia.getString ("nombres", "")+" "+persistencia.getString ("apellidos", ""));
+        id_consumidor.setText (persistencia.getString ("documento", ""));
+        direccion_consumidor.setText (persistencia.getString ("direccion", ""));
+        telefono_consumidor.setText (persistencia.getString ("telefono", ""));
+
     }
 }
